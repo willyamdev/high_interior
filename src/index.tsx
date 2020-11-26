1a96d06e3129382e3c6eaae4ef5267c580d0f71b
Willyam Biancardi <willyamvinicius1900@gmail.com>
2020-11-25 22:18:12 -0300

    added some changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './pages/global/css/global.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CustomTheme from './pages/global/material/material-theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <ThemeProvider theme={CustomTheme}>
+      <Router>
         <App />
-    </Router>
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/global/material/material-theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#0000FF",
+        },
+        secondary: {
+            main: "#FFFFFF"
+        }
+    }
+});
+
+export default theme;
