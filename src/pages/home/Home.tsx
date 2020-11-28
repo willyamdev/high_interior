3950e0bdbfa959fd69ad2ebd2740e56dcf667eb7
Willyam Biancardi <willyamvinicius1900@gmail.com>
2020-11-27 22:49:09 -0300

    added more components to home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Footer from '../global/components/footer/Footer';
 import Benefit from './components/benefit/Benefit';
 import BestInterior from './components/best-interior/BestInterior';
 import Cape from './components/cape/Cape';
 import ExploreGallery from './components/explore-gallery/ExploreGallery';
+import SatisfiedCustomers from './components/satisfied-customers/SatisfiedCustomers';
+import Subscribe from './components/subscribe/Subscribe';
 import CapeImage from './images/home-image.jpg'
 
 export default function Home() {
@@ -13,6 +16,9 @@ export default function Home() {
             <Benefit/>
             <BestInterior/>
             <ExploreGallery/>
+            <SatisfiedCustomers/>
+            <Subscribe/>
+            <Footer/>
         </div>
     );
 }
